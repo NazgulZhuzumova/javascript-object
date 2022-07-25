09b89866fe9e817cbdc123a1c59bb84485a2612b
NazgulZhuzumova <nazgul.zhuzumova@yahoo.co.uk>
2022-07-25 15:42:55 +0100

    commit

---
+++ app.js
@@ -0,0 +1,20 @@
+/*let firstName = 'Cat';
+let lastName = 'James';
+let age = 33;
+let ocupation = "nurse";
+console.log(firstName + " " + lastName + " " + age + " " + ocupation)
+
+let someone = {
+    firstName: "Elli",
+    lastName: "Simon"
+};
+alert(someone.firstName);
+
+*/
+const person = {firstName: 'John', lastName: 'Connery', age: 45, hobby: 'coding'
+}
+console.log(person.firstName + " " + person.lastName + 'is the best worker in the company. He has done an excellent perfomance in his task.')
+
+
+
+
